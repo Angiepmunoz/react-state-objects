91b4889546ea1266657c80d65b28057921f114e7
Angie Munoz <angiepmunoz@gmail.com>
2024-08-07 13:44:13 -0400

    completed up to attendance

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,46 @@
+import { dogsData } from "./data";
+import { useState } from "react";
+
 function App() {
+  const [dogs, setDogs] = useState(dogsData);
+
+  const addDog = () => {
+    // console.log("adding dog")
+    const newDog = {
+      id: dogs.length + 50,
+      name: "Rover",
+      present: false,
+      grade: "100",
+      notes: "The goodest new dog",
+    };
+
+    setDogs([...dogs, newDog]);
+    // we are using our set function to update state
+    // we're creating a new array so react knows that it must rerender the webpage
+    // we are copying the content of the current state with the spread operator and adding the new dog
+  };
+
+  const removeDog = (dogId) => {
+    console.log("removing dog");
+
+    // our new state will be all the dogs that do not match the id that was passed to the function
+    const filteredDogs = dogs.filter((dog) => dog.id !== dogId);
+    setDogs(filteredDogs);
+  };
+
+  const updateDogAttendance = (dogId) => {
+    console.log("update dog attendance for ", dogId);
+    // copying the dogs array so we can update our copy
+    const dogsArr = [...dogs];
+    // find the dog with  the matching id number's array position
+    const index = dogsArr.findIndex((dog)=> dog.id === dogId)
+
+    //access the dogs present property and toggle with the bang operator 
+    dogsArr[index].present = !dogsArr[index].present;
+
+    setDogs(dogsArr);
+  };
+
   return (
     <div className="App">
       <header>
@@ -6,7 +48,36 @@ function App() {
         <h2>{new Date().toDateString()}</h2>
       </header>
       <aside></aside>
-      <main></main>
+      <main>
+        <button onClick={addDog}>Add a new dog</button>
+        <ul>
+          {dogs.map((dog) => {
+            return (
+              <li key={dog.id}>
+                <span
+                  onClick={() => {
+                    updateDogAttendance(dog.id);
+                  }}
+                  style={
+                    dog.present
+                      ? { textDecoration: "none" }
+                      : { textDecoration: "line-through" }
+                  }
+                >
+                  {dog.name}
+                </span>
+                <button
+                  onClick={() => {
+                    removeDog(dog.id);
+                  }}
+                >
+                  remove
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+      </main>
     </div>
   );
 }
